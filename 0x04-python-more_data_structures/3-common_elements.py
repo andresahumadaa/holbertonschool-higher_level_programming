b11be3ad643eb10cc94aa88572378fcfb33fe546
andresahumadaa <2268@holbertonschool.com>
2021-05-07 02:29:02 +0000

    common_elements

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-def only_diff_elements(set_1, set_2):
-    a = list(set_2 - set_1)
-    b = list(set_1 - set_2)
-    return (a + b)
+def common_elements(set_1, set_2):
+    common = set_1.intersection(set_2)
+    return (common)
