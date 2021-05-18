ff0d0afe271fb5bba621fc8ec6bcf49781736ae7
andresahumadaa <2268@holbertonschool.com>
2021-05-18 01:50:20 +0000

    Safe list printing

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''safe_print_list'''
+
+
+def safe_print_list(my_list=[], x=0):
+    '''prints x elements of a list'''
+    a = 0
+    b = 0
+    while a < x:
+        try:
+            print("{}".format(my_list[a]))
+            a += 1
+            b += 1
+        except:
+            break;
+    return b
