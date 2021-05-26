c9c7d9e641370dd4660789706c624c230ea3bb4e
andres ahumada <70974306+andresahumadaa@users.noreply.github.com>
2021-05-25 22:05:17 -0500

    Update 4-print_square.py

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+''' Print square '''
+
+
 def print_square(size):
+    '''prints a square with the character #'''
     if type(size) != int:
         raise TypeError("size must be an integer")
     if size < 0:
