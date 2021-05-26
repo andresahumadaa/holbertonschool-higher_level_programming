5f0bcd0339103c5205ff524af3e7df1bea888818
andres ahumada <70974306+andresahumadaa@users.noreply.github.com>
2021-05-25 21:56:59 -0500

    Update 3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+''' Say my name '''
+
+
 def say_my_name(first_name, last_name=""):
+    ''' prints My name is <first name> <last name> '''
     if type(first_name) != str:
         raise TypeError("first_name must be a string")
     if type(last_name) != str:
